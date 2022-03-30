f532e83d7b9c94ea19d6ef73f2bbe78163890e8a
deepika6232 <90511264+deepika6232@users.noreply.github.com>
2022-03-30 22:12:19 +0530

    Add files via upload

---
+++ Billing_system13/src/billing_system13/Billing_system13.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package billing_system13;
+
+/**
+ *
+ * @author deepika
+ */
+public class Billing_system13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
